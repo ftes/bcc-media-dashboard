6a74cbdf1a1a36146d4fb5f3e15682ca10cfee4b
Fredrik Teschke <f@ftes.de>
2026-09-08 18:33:38 +0200

    Use released Fluffy 0.1.0

--- mix.exs
+++ mix.exs
@@ -46,7 +46,7 @@ defmodule BccmDashboard.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 1.1.0"},
       {:lazy_html, ">= 0.1.0", only: :test},
-      {:fluffy, path: System.get_env("FLUFFY_PATH", "../fluffy"), only: :test},
+      {:fluffy, "~> 0.1.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
